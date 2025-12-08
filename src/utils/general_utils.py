17af0e24ed537dc5a929bd6432f2a57fea2ac384
Song Yi <lydra4@gmail.com>
2025-12-08 10:04:11 +0000

    ci: add pyyaml to additional dependecies for mypy in pre-commit config

---
+++ src/utils/general_utils.py
@@ -0,0 +1,25 @@
+import logging
+import logging.config
+import os
+
+import yaml
+
+logger = logging.getLogger(__name__)
+
+
+def setup_logging(
+    logging_config_path="../conf/logging.yaml", default_level=logging.INFO
+) -> None:
+    try:
+        os.makedirs("logs", exist_ok=True)
+        with open(logging_config_path, encoding="utf-8") as file:
+            log_config = yaml.safe_load(file.read())
+        logging.config.dictConfig(log_config)
+
+    except Exception as error:
+        logging.basicConfig(
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            level=default_level,
+        )
+        logger.info(error)
+        logger.info("Logging config file is not found. Basic config is used.")
