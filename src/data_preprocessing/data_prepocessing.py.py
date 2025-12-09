e25508f2bc542e44a23928a00c094bce55ee2320
Song Yi <lydra4@gmail.com>
2025-12-09 01:41:12 +0000

    feat: add method to data processing class to extract text from epub file

--- src/data_preprocessing/data_prepocessing.py.py
+++ src/data_preprocessing/data_prepocessing.py.py
@@ -1,6 +1,9 @@
 import logging
 from typing import Optional
 
+import ebooklib
+from bs4 import BeautifulSoup
+from ebooklib import epub
 from omegaconf import DictConfig
 
 
@@ -12,3 +15,14 @@ class DataPreprocessing:
     ) -> None:
         self.cfg = cfg
         self.logger = logger or logging.getLogger(__name__)
+
+    def _extract_epub_text(self, path: str) -> str:
+        book = epub.read_epub(name=path)
+        texts = []
+        for item in book.get_items():
+            if item.get_type() == ebooklib.ITEM_DOCUMENT:
+                if item.get_name() not in self.cfg.exclude_files:
+                    soup = BeautifulSoup(item.get_body_content(), "html.parser")
+                    texts.append(soup.get_text(separator="\n"))
+
+        return "\n".join(texts)
