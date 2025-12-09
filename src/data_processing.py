7750adaaeb59a1720a73b5e37106e9940eb2b70f
Song Yi <lydra4@gmail.com>
2025-12-09 06:49:13 +0000

    chore: remove logging config path in data processing entry script since it is the default path in the function

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,24 +1,25 @@
 import logging
-import os
 
 import hydra
 from omegaconf import DictConfig
 
+from data_preprocessing.data_preprocessing import DataPreprocessing
 from utils.general_utils import setup_logging
 
 
 @hydra.main(
     version_base=None,
-    config_path="../conf",
+    config_path="../config",
     config_name="data_processing.yaml",
 )
 def main(cfg: DictConfig):
     logger = logging.getLogger(__name__)
     logger.info("Setting up logging configuration.")
-    setup_logging(
-        logging_config_path=os.path.join(
-            hydra.utils.get_original_cwd(),
-            "conf",
-            "logging.yaml",
-        )
-    )
+    setup_logging()
+
+    data_preprocessing = DataPreprocessing(cfg=cfg, logger=logger)
+    data_preprocessing.perform_processing()
+
+
+if __name__ == "__main__":
+    main()
