397b5be58556fef13efcd9100023374c0c3a3649
Song Yi <lydra4@gmail.com>
2025-12-10 02:45:43 +0000

    feat: completed data processing, yet to train test validation split

--- src/data_preprocessing/data_preprocessing.py
+++ src/data_preprocessing/data_preprocessing.py
@@ -20,23 +20,45 @@ class DataPreprocessing:
         self.cfg = cfg
         self.logger = logger or logging.getLogger(__name__)
 
-    def _extract_clean_epub_books(self, epub_dict: Dict[str, List[str]]):
-        for key, epub_list in epub_dict.items():
-            self.logger.info(f"Processing {key} books.")
-            texts = []
-            for path in tqdm(iterable=epub_list):
-                book = epub.read_epub(name=path)
-                for idref, _ in book.spine:
-                    item = book.get_item_with_id(uid=idref)
+    def _obtain_epub_list(self, path: str) -> Dict[str, List[str]]:
+        walks = [
+            (os.path.basename(dirpath), dirpath, filenames)
+            for dirpath, _, filenames in os.walk(path)
+        ]
 
-                    if item.get_type() == ebooklib.ITEM_DOCUMENT:
-                        if item.get_name() not in self.cfg.exclude_files:
-                            soup = BeautifulSoup(item.get_body_content(), "html.parser")
-                            raw_text = soup.get_text("\n")
-                            clean_text = self._clean_text(text=raw_text)
-                            texts.append(clean_text)
+        return {
+            folder: [
+                os.path.join(dirpath, file)
+                for file in filenames
+                if file.endswith(".epub")
+            ]
+            for folder, dirpath, filenames in walks
+            if folder != ""
+        }
 
-            return "\n".join(texts)
+    def _get_book_number(self, filename: str) -> int:
+        match = re.search(r"(\d+)(?=\.epub$)", filename)
+        if match:
+            return int(match.group(1))
+        self.logger.warning(f"{filename} has no number.")
+        return 9999
+
+    def _extract_epub_books(self, epub_list: List[str]) -> str:
+        epub_list_sorted = sorted(epub_list, key=self._get_book_number)
+        texts = []
+        for epub_path in tqdm(iterable=epub_list_sorted):
+            book = epub.read_epub(name=epub_path)
+            for idref, _ in book.spine:
+                item = book.get_item_with_id(uid=idref)
+                if (
+                    item is not None
+                    and item.get_type() == ebooklib.ITEM_DOCUMENT
+                    and item.get_name() not in self.cfg.exclude_files
+                ):
+                    soup = BeautifulSoup(item.get_body_content(), "html.parser")
+                    texts.append(soup.get_text("\n"))
+
+        return "\n".join(texts)
 
     def _clean_text(self, text: str) -> str:
         cleaned_text = unicodedata.normalize("NFKC", text)
@@ -53,37 +75,35 @@ class DataPreprocessing:
         cleaned_text = re.sub(r"[ \t]+", " ", cleaned_text)
         cleaned_text = re.sub(r"\n{1,}", "\n", cleaned_text)
         cleaned_text = re.sub(
-            r"^\s*(page|chapter)\s*\d+\s*$",
+            r"^\s*(page|chapter)\s*([0-9]+|[A-Z]+|[A-Za-z\-]+)\s*$",
             "",
             cleaned_text,
             flags=re.IGNORECASE | re.MULTILINE,
         )
-        cleaned_text = re.sub(r"-\n\s*", " ", cleaned_text)
+        cleaned_text = re.sub(r"-\n\s*", "", cleaned_text)
         cleaned_text = re.sub(r"([!?.])\1{2,}", r"\1\1\1", cleaned_text)
         cleaned_text = re.sub(
-            r"([^\w\s])\1{2,}",
-            lambda m: m.group(1) * 2,
+            r"\b(?:https?://|www\.)\S+|\b[a-zA-Z0-9.-]+\.(com|org|net|io|co|sg|gov)(/\S*)?",
+            "",
             cleaned_text,
         )
         return cleaned_text.strip()
 
-    def _obtain_epub_list(self, path: str) -> Dict[str, List[str]]:
-        walks = [
-            (os.path.basename(dirpath), dirpath, filenames)
-            for dirpath, _, filenames in os.walk(path)
-        ]
-
-        return {
-            folder: [
-                os.path.join(dirpath, file)
-                for file in filenames
-                if file.endswith(".epub")
-            ]
-            for folder, dirpath, filenames in walks
-            if folder in ("GOT", "HP")
-        }
+    def _save_file(self, processed_path: str, title: str, text: str) -> None:
+        processed_dir = os.path.join(processed_path, title)
+        os.makedirs(name=processed_dir, exist_ok=True)
+        processed_path = f"{processed_dir}/{title}.txt"
+        with open(file=processed_path, mode="w", encoding="utf-8") as f:
+            f.write(text)
 
     def perform_processing(self):
         epub_dict = self._obtain_epub_list(path=self.cfg.raw_epub_dir)
-        texts = self._extract_clean_epub_books(epub_dict=epub_dict)
-        print(texts)
+        for title, epub_list in epub_dict.items():
+            self.logger.info(f"Processing {title} books.")
+            raw_text = self._extract_epub_books(epub_list=epub_list)
+            clean_text = self._clean_text(text=raw_text)
+            self._save_file(
+                processed_path=self.cfg.processed_dir,
+                title=title,
+                text=clean_text,
+            )
