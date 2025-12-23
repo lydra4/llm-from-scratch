f99baf12661d6c16943aa3d14911373e11b67506
Song Yi <lydra4@gmail.com>
2025-12-23 18:00:50 +0800

    feat: added method convert text to bytes and initialize vocab, plus orchestrator method

--- src/tokenization/generate_tokens.py
+++ src/tokenization/generate_tokens.py
@@ -1,9 +1,6 @@
 import logging
 import os
-from typing import (
-    List,
-    Optional,
-)
+from typing import Any, Dict, List, Optional, Tuple
 
 from omegaconf import DictConfig
 
@@ -21,7 +18,34 @@ class GenerateTokens:
         with open(file=train_path, mode="r", encoding="utf-8") as f:
             self.train_text = f.read()
 
-    def _convert_text_to_bytes(self, text: str, encoding: str) -> List[int]:
-        byte_data = text.encode(encoding)
-        tokens = list(byte_data)
-        return tokens
+    def _convert_text_to_bytes(
+        self,
+        text: str,
+        encoding: str,
+    ) -> Tuple[List[bytes], List[int]]:
+        byte_sequence = text.encode(encoding)
+        byte_content = [bytes([byte]) for byte in byte_sequence]
+
+        token_ids = list(byte_sequence)
+        return byte_content, token_ids
+
+    def _init_vocab(
+        self,
+        byte_content: List[bytes],
+        token_ids: List[int],
+    ) -> Dict[bytes, int]:
+        unique_byte_content = list(set(byte_content))
+        unique_token_ids = list(set(token_ids))
+        token_to_id = {
+            byte_content: token_id
+            for byte_content, token_id in zip(unique_byte_content, unique_token_ids)
+        }
+        return token_to_id
+
+    def tokenize_text(self) -> Any:
+        byte_content, token_ids = self._convert_text_to_bytes(
+            text=self.train_text,
+            encoding=self.cfg.character_encoding,
+        )
+        token_to_id = self._init_vocab(byte_content=byte_content, token_ids=token_ids)
+        print(token_to_id)
