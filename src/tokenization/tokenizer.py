33bf47400c604112cdabb50273e31ac5cfa72d34
Ong Song Yi <ong_song_yi@aiap.sg>
2026-03-28 08:46:24 +0800

    feat: tokenizer class

---
+++ src/tokenization/tokenizer.py
@@ -0,0 +1,12 @@
+import logging
+from typing import Optional
+
+from omegaconf import DictConfig
+
+
+class Tokenzer:
+    def __init__(
+        self, cfg: DictConfig, logger: Optional[logging.Logger] = None
+    ) -> None:
+        self.cfg = cfg
+        self.logger = logger or logging.getLogger(__name__)
