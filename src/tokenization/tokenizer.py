64d37dfc59e0ca69c579ed0c21fe1e7dcc2c0f10
Ong Song Yi <ong_song_yi@aiap.sg>
2026-04-25 10:43:52 +0800

    feat: add parse vocab json, yield dataset paths, encode all text to tokenizer class

--- src/tokenization/tokenizer.py
+++ src/tokenization/tokenizer.py
@@ -1,9 +1,12 @@
 import ast
 import json
 import logging
-from typing import Optional
+from os import PathLike
+from pathlib import Path
+from typing import Iterator, Optional
 
 from omegaconf import DictConfig
+from tqdm import tqdm
 
 
 class Tokenizer:
@@ -13,17 +16,36 @@ class Tokenizer:
         self.cfg = cfg
         self.logger = logger or logging.getLogger(__name__)
 
+    def _parse_vocab_json(
+        self,
+        vocab_path: str | PathLike,
+        mode: str = "r",
+        encoding: str = "utf-8",
+    ) -> dict[bytes, int]:
         with open(
-            file=self.cfg.vocab_path,
-            mode="r",
-            encoding=self.cfg.character_encoding,
+            file=vocab_path,
+            mode=mode,
+            encoding=encoding,
         ) as f:
             raw_vocab = json.load(f)
 
-        self.vocab = {ast.literal_eval(k): int(v) for k, v in raw_vocab.items()}
-        self.inverse_vocab = {v: k for k, v in self.vocab.items()}
+        return {ast.literal_eval(k): int(v) for k, v in raw_vocab.items()}
 
-    def encode(
+    def _yield_dataset_paths(
+        self,
+        data_path: str | PathLike,
+        extension: str = ".txt",
+        encoding: str = "utf-8",
+    ) -> Iterator[tuple[str, str]]:
+        paths = Path(data_path).rglob(f"*{extension}")
+
+        for path in paths:
+            folder_name = path.parent.name
+            text_content = path.read_text(encoding=encoding)
+
+            yield folder_name, text_content
+
+    def _encode(
         self,
         text: str,
         vocab: dict[bytes, int],
@@ -32,23 +54,26 @@ class Tokenizer:
         text_bytes = text.encode(encoding=encoding)
         tokens = [bytes([b]) for b in text_bytes]
 
-        while len(tokens) >= 2:
-            lowest_pair_idx = None
-            lowest_vocab_id = float("inf")
+        with tqdm(total=(len(tokens) - 1), desc="Merging tokens", unit="iter") as pbar:
+            while len(tokens) >= 2:
+                lowest_pair_idx = None
+                lowest_vocab_id = float("inf")
 
-            for i in range(len(tokens) - 1):
-                pair = tokens[i] + tokens[i + 1]
+                for i in range(len(tokens) - 1):
+                    pair = tokens[i] + tokens[i + 1]
 
-                if pair in vocab and vocab[pair] < lowest_vocab_id:
-                    lowest_vocab_id = vocab[pair]
-                    lowest_vocab_id = i
+                    if pair in vocab and vocab[pair] < lowest_vocab_id:
+                        lowest_pair_idx = i
+                        lowest_vocab_id = vocab[pair]
 
-            if lowest_pair_idx is None:
-                break
+                if lowest_pair_idx is None:
+                    break
 
-            i = lowest_pair_idx
-            merged_token = tokens[i] + tokens[i + 1]
-            tokens = tokens[:i] + [merged_token] + tokens[i + 2 :]
+                i = lowest_pair_idx
+                merged_token = tokens[i] + tokens[i + 1]
+                tokens = tokens[:i] + [merged_token] + tokens[i + 2 :]
+
+                pbar.update(1)
 
         return [vocab[t] for t in tokens if t in vocab]
 
@@ -60,3 +85,14 @@ class Tokenizer:
     ) -> str:
         byte_sequence = b"".join([inverse_vocab.get(idx, b"") for idx in token_ids])
         return byte_sequence.decode(encoding=encoding, errors="replace")
+
+    def encode_all_text(self):
+        vocab = self._parse_vocab_json(vocab_path=self.cfg.vocab_path)
+
+        for split_name, text in tqdm(
+            self._yield_dataset_paths(data_path=self.cfg.data_path),
+            desc="Overall Progress",
+        ):
+            self.logger.info(f"Encoding '{split_name}' file.")
+            tokens_ids = self._encode(text=text, vocab=vocab)
+            print(tokens_ids)
