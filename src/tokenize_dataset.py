7ad50dd0785eed03601dd4bcfb5545253405b727
Song Yi <lydra4@gmail.com>
2026-04-28 09:58:20 +0800

    chore: rename prepare dataset entry script to tokenize dataset, in addition, fix import statements and change config name to reflect changes

--- src/tokenize_dataset.py
+++ src/tokenize_dataset.py
@@ -3,22 +3,22 @@ import logging
 import hydra
 from omegaconf import DictConfig
 
-from tokenizer.tokenizer import Tokenizer
+from tokenizer.tokenizer import BPETokenizer
 from utils.general_utils import setup_logging
 
 
 @hydra.main(
     version_base=None,
     config_path="../config",
-    config_name="prepare_dataset.yaml",
+    config_name="tokenize_dataset.yaml",
 )
 def main(cfg: DictConfig):
     logger = logging.getLogger(__name__)
     logger.info("Setting up logging configuration.")
     setup_logging()
 
-    tokenizer = Tokenizer(cfg=cfg, logger=logger)
-    tokenizer.encode_all_text()
+    bpe_tokenizer = BPETokenizer(cfg=cfg, logger=logger)
+    bpe_tokenizer.encode_all_text()
 
 
 if __name__ == "__main__":
