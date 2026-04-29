b4709df591b6d451bc5437d73aae794a2ad3b833
Song Yi <lydra4@gmail.com>
2026-04-29 14:37:41 +0800

    chore: load in dataset for both train and validation in train entry script

--- src/train.py
+++ src/train.py
@@ -2,6 +2,7 @@ import logging
 
 import hydra
 from omegaconf import DictConfig
+from torch.utils.data import DataLoader
 
 from dataset.token_dataset import TokenDataset
 from utils.general_utils import setup_logging
@@ -17,9 +18,21 @@ def main(cfg: DictConfig):
     logger.info("Setting up logging configuration.")
     setup_logging()
 
-    token_dataset = TokenDataset(cfg=cfg, logger=logger)
-    print(token_dataset)
+    train_dataset = TokenDataset(
+        data_path=cfg.train.data_path,
+        context_window=cfg.dataset.context_window,
+        logger=logger,
+    )
+    train_loader = DataLoader(dataset=train_dataset, **cfg.train.loader)
+
+    val_dataset = TokenDataset(
+        data_path=cfg.val.data_path,
+        context_window=cfg.dataset.context_window,
+        logger=logger,
+    )
+    val_loader = DataLoader(dataset=val_dataset, **cfg.val.loader)
+    print(train_loader, val_loader)
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
