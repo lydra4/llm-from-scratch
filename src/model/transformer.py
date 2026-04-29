b89d4b13be3d7b2ea6db64d98210d29b7fab624a
Ong Song Yi <ong_song_yi@aiap.sg>
2026-04-29 19:32:12 +0800

    refactor: inherit from torch.nn in transformer class

--- src/model/transformer.py
+++ src/model/transformer.py
@@ -2,9 +2,11 @@ import logging
 from typing import Optional
 
 from omegaconf import DictConfig
+from torch import nn
 
 
-class TransformerLM:
-    def __init__(self, cfg: DictConfig, logger: Optional[logging.Logger,]) -> None:
+class TransformerLM(nn.Module):
+    def __init__(self, cfg: DictConfig, logger: Optional[logging.Logger]) -> None:
+        super().__init__()
         self.cfg = cfg
         self.logger = logger or logging.getLogger(__name__)
