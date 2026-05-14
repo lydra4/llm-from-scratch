fe5064cca9b3dcfeb3eeef29dcedee9545bedb04
Song Yi <lydra4@gmail.com>
2026-05-14 14:13:24 +0800

    feat: implement transformer block with pre-layer normalization and residual connections

---
+++ src/model/modules/block.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+from omegaconf import DictConfig
+
+from model.mlp import FeedForward
+from model.modules.attention import CausalSelfAttention
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg: DictConfig) -> None:
+        super().__init__()
+        self.cfg = cfg
+
+        self.ln_1 = nn.LayerNorm(normalized_shape=self.cfg.model.d_model)
+        self.ln_2 = nn.LayerNorm(normalized_shape=self.cfg.model.d_model)
+
+        self.attention = CausalSelfAttention(cfg=cfg)
+        self.mlp = FeedForward(cfg=cfg)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = x + self.attention(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+
+        return x
