673104a4c721c67d3bcd88aaa269bb7a23e87bc6
Song Yi <lydra4@gmail.com>
2026-05-14 14:12:44 +0800

    feat: add feed forward network with gelu activation in mlp.py

---
+++ src/model/mlp.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+from omegaconf import DictConfig
+
+
+class FeedForward(nn.Module):
+    def __init__(self, cfg: DictConfig) -> None:
+        super().__init__()
+        self.cfg = cfg
+
+        self.d_model = self.cfg.model.d_model
+        hidden_dim = 4 * self.d_model
+        dropout = self.cfg.model.dropout
+        self.net = nn.Sequential(
+            nn.Linear(in_features=self.d_model, out_features=hidden_dim),
+            nn.GELU(),
+            nn.Linear(in_features=hidden_dim, out_features=self.d_model),
+            nn.Dropout(p=dropout),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.net(x)
